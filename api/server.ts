5a8593cbeffa96de2c7e2c8f917bfa64fbc285b4
yuvaraj949 <yuvarajnayak2@gmail.com>
2025-07-07 15:18:17 +0400

    update sender mailing agent to Brevo SMTP relay

--- api/server.ts
+++ api/server.ts
@@ -71,16 +71,12 @@ const generateRegistrationId = async (): Promise<string> => {
 };
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
+  host: 'smtp-relay.brevo.com',
   port: 465,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
 
